b545dd2c3a4af3dfe5b703ea722783fb1b38d592
tanvirsp <tanvirgraphic@gmail.com>
2021-05-25 12:28:42 +0600

    assignment is done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,6 @@
+// https://github.com/tanvirsp/assignment
+
+
 //kilometerToMeter
 function kilometerToMeter(kilometer){
     if(kilometer < 0 ){
@@ -14,22 +17,42 @@ function kilometerToMeter(kilometer){
 
 //watch price 50,   phone price 100,  laptop price 500
 function budgetCalculator(watch, phone, laptop){
-    var watchPrice = watch * 50;
-    var phonePrice = phone * 100;
-    var laptopPrice = laptop * 500;
+    //check nagetive value for watch
+    if (watch < 0 ){
+        return "Nagetive Watch Number is not allow"
+    } 
+    else {
+        var watchPrice = watch * 50;    
+    }
+
+    //check nagetive value for Phone
+    if(phone < 0){
+        return "Nagetive Phone Number is not allow"
+    }
+    else {
+        var phonePrice = phone * 100;    
+    }
+    //check nagetive value for Laptop
+    if(laptop < 0){
+        return "Nagetive Laptop Number is not allow"
+    }
+    else {
+        var laptopPrice = laptop * 500;
+    }
     var total = watchPrice + phonePrice + laptopPrice;
     return total;
-
 }
+
+
 
 
 
 // 1-10 day = 100,   11-20 day = 80,  21 or above day = 50
 function hotelCost(days){
     var totalCost = 0;
+
     if (days < 0){
         return "Please put Valid Number"
-
     }
     else if( days <= 10){
         var firstSession = days * 100;
@@ -54,14 +77,26 @@ function hotelCost(days){
 
 
 
+
+
+
 //find megaFriend
 function megaFriend(friends){
     var megaName  = friends[0];
-    for(var i = 0; i < friends.length; i++ ){
-        var element = friends[i];
-        if(element.length > megaName.length){
-            megaName = element;
-        }
+
+    //check empty friends arry
+    if(friends.length == 0 || friends[0] == ""){
+        return "Enpty Array List is not allow"
+    }
+    else {
+        var megaName  = friends[0];
+        for(var i = 0; i < friends.length; i++ ){
+            var element = friends[i];
+            if(element.length > megaName.length){
+                megaName = element;
+            }
     }
     return megaName;
+    }
 }
+
